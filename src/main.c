56c03ad1a5ea29995318c01030232fc0cc97c4c5
Antoine <me@antoinebellanger.fr>
2025-01-21 18:53:46 +0100

    fix(cs): whole main

--- src/main.c
+++ src/main.c
@@ -13,27 +13,10 @@
 int main(void)
 {
     string_t s;
-    string_t s2;
-    char *test_str = calloc(1, 10);
 
-    string_init(&s, " ");
-    string_init(&s2, "llo");
-    s.assign_c(&s, "Hello World");
-    s.append_c(&s, "!!");
-    s.copy(&s, test_str, 6, 1);
-    printf("COPY: %s\n", test_str);
-    printf("%s\n", s.str);
-    printf("%c\n", s.at(&s, 2));
-    printf("FIND_S: %d + 1 = 2\n", s.find_s(&s, &s2, 1));
-    printf("FIND_C: %d\n", s.find_c(&s, "Hello World", 0));
-    printf("PRINT BUFF: %s\n", s.c_str(&s));
-    printf("IS EMPTY: %d\n", s.empty(&s));
-    printf("Length: %d\n", s.length(&s));
-    s.clear(&s);
-    printf("Content: %s\nSize: %d\n", s.str, s.length(&s));
-    s.assign_c(&s, "- :: -");
-    s.insert_c(&s, 3, "Hi there");
-    printf("INSERT_C: %s\n", s.str);
+    string_init(&s, "abcdefg");
+    s.assign_c(&s, "42");
+    printf("TO_INT: %d\n", s.to_int(&s));
     string_destroy(&s);
     return (0);
 }
